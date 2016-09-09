250e37d211cba4820fc9012148771a0ddad25805
Scott Sampson <Scott-Sampson@users.noreply.github.com>
2016-09-09 21:09:12 +0100

    Add Angel's Refining building info
    I *think* I got them all...

---
+++ mod-compadibility/angels-mods.lua
@@ -0,0 +1,36 @@
+--angelsrefining
+--1x1, 2x2, 3x3, 4x4, 5x5, 9x10, custom
+	function check_angelsrefining(mod)
+		if (mod == "angelsrefining") then
+		add_to_table({
+			{"2x2", "liquifier"},
+			{"2x2", "liquifier-2"},
+			{"3x3", "burner-ore-crusher"},
+			{"3x3", "ore-crusher"},
+			{"3x3", "ore-crusher-2"},
+			{"3x3", "ore-crusher-3"},
+			{"3x3", "clarifier"},
+			{"4x4", "crystallizer"},
+			{"4x4", "crystallizer-2"},
+			{"4x4", "ore-leaching-plant"},
+			{"4x4", "ore-leaching-plant-2"},
+			{"4x4", "ore-leaching-plant-3"},
+			{"4x4", "ore-floatation-cell"},
+			{"4x4", "ore-floatation-cell-2"},
+			{"4x4", "ore-floatation-cell-3"},
+			{"4x4", "filtration-unit"},
+			{"4x4", "filtration-unit-2"},
+			
+			{"4x4", "ore-refinery"},
+			{"4x4", "ore-refinery-2"},
+			{"4x4", "ore-refinery-3"},
+			{"4x4", "hydro-plant"},
+			{"4x4", "hydro-plant-2"},
+			{"5x5", "ore-sorting-facility"},
+			{"5x5", "ore-sorting-facility-2"},
+			{"5x5", "ore-sorting-facility-3"},
+			{"5x5", "ore-sorting-facility-4"},
+			{"5x5", "thermal-extractor"}
+		})
+	end
+end	
