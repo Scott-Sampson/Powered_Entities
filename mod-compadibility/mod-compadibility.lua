4c388502b79b83c46769720c884389f45b323dd0
Scott Sampson <Scott-Sampson@users.noreply.github.com>
2016-09-09 21:08:21 +0100

    Update to add Angel's Refining check

--- mod-compadibility/mod-compadibility.lua
+++ mod-compadibility/mod-compadibility.lua
@@ -1,4 +1,5 @@
 require "bobs-mods"
+require "angels-mods"
 
 --Format for each entry in the table is {"entity_type", "entity_name"}
 --Valid entity_types are as follows:
@@ -15,6 +16,7 @@ function modCompadibility()
 		check_bobpower(mod)
 		check_bobtech(mod)
 		check_bobwarfare(mod)
+		check_angelsrefining(mod)
 	end
 end
 
